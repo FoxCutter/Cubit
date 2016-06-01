ecfe525b3f276d5fcf20287a364c1809294f1fca
Fox Cutter <FoxCutter@gmail.com>
2016-05-31 23:30:55 -0700

    Updated file

---
+++ Tools/ZASM/SymbolTable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZASM
+{
+    class SymbolTableEntry
+    {
+        public string Symbol { get; set; }
+        public List<int> LineIDs { get; set; }
+
+        public SymbolTableEntry(string Name = "")
+        {
+            Symbol = Name;
+            LineIDs = new List<int>();
+        }
+    };
+
+    class SymbolTable : IEnumerable<SymbolTableEntry>
+    {
+        Dictionary<string, SymbolTableEntry> NameList = new Dictionary<string, SymbolTableEntry>();
+        SymbolTable Parent = null;
+
+        public SymbolTable() 
+        {
+        }
+
+        public SymbolTableEntry this[string Name]
+        {
+            get 
+            {
+                if (!NameList.ContainsKey(Name.ToUpper()))
+                {
+                    NameList[Name.ToUpper()] = new SymbolTableEntry(Name);
+                }
+
+                return NameList[Name.ToUpper()];
+            }
+            
+            set 
+            {
+                NameList[Name.ToUpper()] = value;            
+            }
+        }
+
+        public IEnumerator<SymbolTableEntry> GetEnumerator()
+        {
+            return NameList.Values.OrderBy(e => e.Symbol.ToUpper()).GetEnumerator();
+
+            //return NameList.Values.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return NameList.Values.GetEnumerator();
+        }
+    }
+}
